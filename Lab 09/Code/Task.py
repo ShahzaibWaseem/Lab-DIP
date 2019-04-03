ad1123aab44ada33afbc03a8b61c51a6f92bb253
ShahzaibWaseem <shahzaib.waseem96@gmail.com>
2019-04-03 22:53:48 +0500

    Adding Lab 09 - Task 4 Median Smoothing - Code and Images

--- Lab 09/Code/Task.py
+++ Lab 09/Code/Task.py
@@ -17,10 +17,23 @@ def Smoothening(image, filter):
 			newImage[x-padSize,y-padSize] = np.sum(np.multiply(pixels, filter/filterSum))
 	return newImage
 
+def medianSmoothing(image, windowSize):
+	height, width = image.shape
+	padSize = windowSize // 2		# Integer Division
+	newImage = np.zeros(image.shape)
+	image = np.pad(image, (padSize, padSize), 'constant', constant_values=(0) )
+
+	for x in range(padSize, height - padSize):
+		for y in range(padSize, width - padSize):
+			pixels = image[x-padSize:x+padSize+1, y-padSize:y+padSize+1]
+			pixels = np.sort(pixels, axis=None)
+			newImage[x-padSize, y-padSize] = pixels[(windowSize**2) // 2]
+	return newImage
+
 def main():
 	IMAGE_PATH = "../Images/"
-	smoothing_image = "unsharpmasking.tif"
-	windowSize = 3			# Change this to whatever filter you require
+	smoothing_image = "saltandpaper.tif"
+	# windowSize = 7			# Change this to whatever filter you require
 
 	image = cv2.imread(IMAGE_PATH + smoothing_image, 0)
 	# Task 1a Filter
@@ -41,10 +54,13 @@ def main():
 	# gaussianFilter = gaussianFilter / np.sum(gaussianFilter * sigma)
 
 	# Task 3 Filter
-	filter = np.array([ [1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.int32)
-	newImage = Smoothening(image, filter)
+	# filter = np.array([ [1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.int32)
+	# filter = np.ones((windowSize, windowSize))
 
-	newImage = image + (image - newImage)
+	# newImage = Smoothening(image, filter)
+	# newImage = image + (image - newImage)
+
+	newImage = medianSmoothing(image, 3)	# Filter Size = 3x3
 
 	stackedImage = np.hstack((image, newImage))
 	cv2.imwrite(IMAGE_PATH + "output " + smoothing_image.split(".")[0] + ".png", newImage)
